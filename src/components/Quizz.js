a0608f8fb6de27a8f177e29241c68a038096f4d9
Gilles Avraam <gillouavraam@gmail.com>
2021-02-07 20:36:25 +0100

    [add]: Quizz choix des selections, score, verification
    Bug à corriger : relancement des reponses aléatoire
    Ajouter autre quizz

--- src/components/Quizz.js
+++ src/components/Quizz.js
@@ -1,49 +1,72 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
+import '../sass/component/_cards.scss';
 
 
-
-const TableBody = (props, {question, options}) => {
-        
-    const [answers, setAnswer] = useState(options);
-
-    const Ligne = props.infoData.map((data, index) => {
+const QuizzQuestions = React.memo((props, checked, onchange) => {
+    const [count, setCount]= useState(0);
+    const [valid, setValid]= useState(false);
+    useEffect(() =>{
+        document.querySelector('#score').innerHTML = `Vous avez ${count} bonne réponse !`;
+    });
+    
+    const Question = props.infoData.map((data, index) => {
         let arrayLenght = props.infoData.length + 1;
         var randomAnswers = data.answers.sort(() => 0.5 - Math.random()).slice(0, arrayLenght);
-
+       
+     
         return(
             
-                <section key={index}>
+                <fieldset key={index}>
                     {/* <td>{data.id}</td> */}
-                    <pre>{data.id}. {data.question}</pre>
+                    <legend>{data.id}. {data.question}</legend>
                 
                     {randomAnswers.map((value, i) => {
                         return (
                             <>
-                                 <input type="radio" id={"answers_"+ data.id +"_"+i} name={"answers_"+ data.id} value={value}></input>
-                                <label for={"answers_"+ data.id +"_"+i}>{value}</label><br></br>
+                                <label >
+                                    <input onClick={() => (value === data.correct) ? (setCount(count + 1)):setCount(count - 1)}
+                                     className={(value === data.correct) ? "radioButton Valid": "radioButton" } type="radio" 
+                                     id={"answers_"+ data.id +"_"+i} name={"answers_"+ data.id}
+                                     value={value}></input>
+
+                                    {value}
+                                </label><br/>
                             </>
                         )
                     })}
 
                     
                     {/* <td>{data.correct}</td> */}
-                </section>
+                </fieldset>
              
             
         );
     });
 
-    return <tbody>{Ligne}</tbody>;
-}
+    return <form>{Question}</form>;
+})
+
+function ShowResult() {
+    document.querySelector('#score').style.display = "block";
+    document.styleSheets[0].insertRule('.Valid::after { display: block !important; }', 0);
+} 
 
 
 
 const Quizz = (props) => {
     const { infoData } = props;
+
+    const [question, setQuestions] = useState([]);
+    useEffect(()=>{
+        setQuestions(infoData)
+    })
         return(
-            <table border="1" align="center">
-                <TableBody infoData={infoData} />
-            </table>
+            <section border="1" align="center">
+                <QuizzQuestions infoData={infoData} />
+                <hr/>
+                <button onClick={ShowResult}>Voir mon score</button>
+                <h3 id="score" style={{display: "none"}}></h3>
+            </section>
         )
     }
 
